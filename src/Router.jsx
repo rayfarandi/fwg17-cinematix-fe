d8b516563551e649cfc0e57e5246f41b3addd843
Ran Ran <rayfarandi1994@gmail.com>
2024-03-06 09:33:04 +0700

    Update Dropdown menu

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,9 +2,9 @@ import { createBrowserRouter } from "react-router-dom";
 
 import Home from "./pages/Home";
 import MovieDetail from "./pages/MovieDetail";
-import Order from "./pages/Order";
-import Payment from "./pages/Payment";
-import TicketResult from "./pages/TicketResult";
+// import Order from "./pages/Order";
+// import Payment from "./pages/Payment";
+// import TicketResult from "./pages/TicketResult";
 
 const router = createBrowserRouter([
   {
@@ -15,18 +15,18 @@ const router = createBrowserRouter([
     path: "/movie/:id",
     element: <MovieDetail />,
   },
-  {
-    path: "/order",
-    element: <Order />,
-  },
-  {
-    path: "/payment",
-    element: <Payment />,
-  },
-  {
-    path: "/ticketresult",
-    element: <TicketResult />,
-  },
+  // {
+  //   path: "/order",
+  //   element: <Order />,
+  // },
+  // {
+  //   path: "/payment",
+  //   element: <Payment />,
+  // },
+  // {
+  //   path: "/ticketresult",
+  //   element: <TicketResult />,
+  // },
 ]);
 
 export default router;
